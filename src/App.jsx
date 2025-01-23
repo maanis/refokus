aa61418497958e21288e9b0dd7a82f46d8b46b29
Manish Jha <manishjha962@gmail.com>
2025-01-23 21:11:43 +0530

    add Work component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 const App = () => {
     return (
         <>
             <div className='h-screen w-full bg-zinc-900'>
                 <Navbar />
+                <Work />
             </div>
         </>
     )
