dec16b61540a0d4ce70f41a141f63f65d01d2b85
Manish Jha <manishjha962@gmail.com>
2025-01-23 21:12:13 +0530

    enhance styling of Work component and update HTML structure

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Work = () => {
     return (
-        <div>Work</div>
+        <div className='max-w-screen-lg select-none text-center text-white py-6 mx-auto'>
+            <div className='text-[30vw] leading-none tracking-tight font-medium font-sans   '>work</div>
+        </div>
     )
 }
 
