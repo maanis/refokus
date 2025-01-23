02d5d9a00844e215ed2b24b3935f16f2c55226fc
Manish Jha <manishjha962@gmail.com>
2025-01-23 21:02:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+    return (
+        <>
+            <div className='h-screen w-full bg-zinc-900'>
+                <Navbar />
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoIosReturnRight } from "react-icons/io";
+
+const Button = () => {
+    return (
+        <button className='flex justify-between items-center gap-6 bg-white px-3 py-[6px] rounded-full'>
+            <span className='text-xs font-semibold'>Get Started</span>
+            <IoIosReturnRight />
+        </button>
+    )
+}
+
+export default Button
