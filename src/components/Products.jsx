4d9e9764aa96497b04d392081f849f2315c0fd0b
Manish Jha <manishjha962@gmail.com>
2025-01-24 20:49:20 +0530

    replace Products component content with Product component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Product from './Product'
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
+    return (
+        <Product />
+    )
 }
 
 export default Products
