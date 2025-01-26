90a0038b9956d47470b400e230ea6ee6aafaf825
Manish Jha <manishjha962@gmail.com>
2025-01-26 17:23:35 +0530

    refactor Marq and Work components; adjust animation duration and improve image rendering for better performance and visual consistency

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -49,7 +49,7 @@ const Work = () => {
             <div className="absolute  top-[-60px] w-[300px] left-[-200px] h-[300px] " style={{ background: 'radial-gradient(circle, rgba(0, 255, 170, 0.3), rgba(0, 0, 0, 0))', filter: 'blur(135px)', zIndex: 0, }}></div>
             <div className="absolute w-[300px] right-[-200px] bottom-[-70px] h-[300px] " style={{ background: 'radial-gradient(circle, rgba(0, 255, 170, 0.3), rgba(0, 0, 0, 0))', filter: 'blur(135px)', zIndex: 0, }}></div>
             <div className='text-[30vw] leading-none tracking-tight font-medium font-sans max-md:text-[40vw]'>work</div>
-            {images.map((img, index) => img.isActive && <img key={index} src={img.url} className={`absolute z-${img.z} w-72 max-lg:w-56 max-md:w-40 max-sm:w-36 rounded-lg object-cover -translate-x-1/2 -translate-y-1/2`} style={{ top: img.top, left: img.left }} />)}
+            {images.map((img, index) => img.isActive && <img key={index} src={img.url} className={`absolute z-${img.z} w-72 max-lg:w-56 max-md:w-40  rounded-lg object-cover -translate-x-1/2 -translate-y-1/2`} style={{ top: img.top, left: img.left }} />)}
         </div>
     )
 }
