56f62aedf682e6eda0eb5e2648e2bf15096a1de7
Manish Jha <manishjha962@gmail.com>
2025-01-26 16:52:18 +0530

    refactor Button and Product components; enhance responsive design with improved styling and layout adjustments for better visual consistency

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,7 +19,7 @@ const Products = () => {
         {
             id: 2,
             title: 'cula',
-            description: `We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.`,
+            description: `We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes.`,
             btn2: false,
             gradient: `bg-[radial-gradient(circle,_rgba(238,174,202,1)_0%,_rgba(148,187,233,1)_100%)]`
         },
@@ -33,22 +33,22 @@ const Products = () => {
         {
             id: 4,
             title: 'YIR',
-            description: `We enhanced the New York Fashion Week, by creating a fully digital AR fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models and an integrated web shop.`,
+            description: `We enhanced the New York Fashion Week, by creating a fully digital AR fashion experience for Yahoo and Maisie Wilen.`,
             btn2: false,
-            gradient: `bg-[linear-gradient(0deg,_rgba(34,193,195,1)_0%,_rgba(253,187,45,1)_100%)]`
+            gradient: `bg-gradient-to-r from-yellow-400 via-red-500 to-orange-400`
         }
 
     ]
     const [pos, setpos] = useState({ y: 0, show: false })
 
     return (
-        <div className='mt-24 relative'>
+        <div className='mt-24 max-md:mt-16 max-sm:mt-10 relative'>
             {products.map((product, index) => (
                 <div key={index}>
                     <Product title={product.title} show={pos.show} index={index} gradient={product.gradient} setPos={setpos} description={product.description} btn2={product.btn2} />
                     <div className="absolute h-full w-full top-[0] pointer-events-none">
 
-                        <motion.div initial={{ y: 0, x: "-50%" }} transition={{ ease: [0.87, 0, 0.13, 1], duration: .6 }} animate={{ y: pos.y + `rem` }} className="window h-[14rem] w-80 absolute overflow-hidden pointer-events-none  left-[45%] -translate-x-1/2">
+                        <motion.div initial={{ y: 0, x: "-50%" }} transition={{ ease: [0.87, 0, 0.13, 1], duration: .6 }} animate={{ y: pos.y + `rem` }} className="window h-[14rem] max-sm:w-48 w-80 max-md:w-64 absolute overflow-hidden pointer-events-none  left-[45%] max-[1190px]:left-[46%] max-[890px]:left-[43%] max-md:left-[42%] max-sm:left-[74%] max-[390px]:left-[72%] max-[390px]:w-40 -translate-x-1/2">
                             <motion.div animate={{ y: -pos.y + `rem` }} className=" h-full w-full"><video width={"450px"} transition={{ ease: [0.87, 0, 0.13, 1], duration: .5 }} src={video1} autoPlay muted loop className={`w-full h-full rounded-lg object-cover ${!pos.show ? 'hidden' : ''}`}></video></motion.div>
                             <motion.div animate={{ y: -pos.y + `rem` }} className=" h-full w-full"><video width={"450px"} transition={{ ease: [0.87, 0, 0.13, 1], duration: .5 }} src={video2} autoPlay muted loop className={`w-full h-full rounded-lg object-cover ${!pos.show ? 'hidden' : ''}`}></video></motion.div>
                             <motion.div animate={{ y: -pos.y + `rem` }} className=" h-full w-full"><video width={"450px"} transition={{ ease: [0.87, 0, 0.13, 1], duration: .5 }} src={video3} autoPlay muted loop className={`w-full h-full rounded-lg object-cover ${!pos.show ? 'hidden' : ''}`}></video></motion.div>
