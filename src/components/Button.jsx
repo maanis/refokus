6acde1f54e458c05132f162b33cd910f6673e640
Manish Jha <manishjha962@gmail.com>
2025-01-26 13:40:03 +0530

    refactor Button and Navbar components; add hide prop to Button for conditional rendering and enhance Navbar layout for better responsiveness

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { IoIosReturnRight } from "react-icons/io";
 
-const Button = ({ text = 'Get Started' }) => {
+const Button = ({ text = 'Get Started', hide = false }) => {
     return (
-        <button className='flex justify-between items-center gap-6 bg-white px-3 py-[6px] rounded-full'>
+        <button className={`flex justify-between items-center gap-6 bg-white px-3 py-[6px] rounded-full max-md:${hide ? 'hidden' : 'block'}`}>
             <span className='text-sm text-black font-medium'>{text}</span>
             <IoIosReturnRight className='text-black' />
         </button>
