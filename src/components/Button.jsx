4edaec3696ecc77e85a1f60fac33b28b628acdcd
Manish Jha <manishjha962@gmail.com>
2025-01-26 17:37:33 +0530

    refactor Button and Footer components; enhance responsive design with improved styling and layout adjustments for better visual consistency

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { IoIosReturnRight } from "react-icons/io";
 
 const Button = ({ text = 'Get Started', hide = false }) => {
     return (
-        <button className={`flex justify-between items-center bg-white px-3  max-sm:px-2 max-sm:py-[3px] py-[6px] rounded-full max-md:${hide && 'hidden'}`}>
+        <button className={`flex justify-between items-center bg-white px-3 md:w-32 max-sm:px-2 max-sm:py-[3px] py-[6px] rounded-full max-md:${hide && 'hidden'}`}>
             <span className='text-sm text-black font-medium whitespace-nowrap max-[400px]:text-xs'>{text}</span>
             <IoIosReturnRight className='text-black max-[400px]:text-xs' />
         </button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='w-full py-8 relative'>
+        <div className='w-full py-8 max-md:py-4 max-md:pb-6 relative'>
             <div className="absolute w-[300px] right-[-230px] top-[-180px] h-[300px] " style={{ background: 'radial-gradient(circle, rgba(0, 255, 170, 0.5), rgba(0, 0, 0, 0))', filter: 'blur(125px)', zIndex: 0, }}></div>
 
-            <div className='max-w-screen-lg mx-auto flex items-center  gap-8'>
-                <div className=''>
-                    <h2 className='text-[7rem] font-semibold leading-none tracking-tighter'>ReFokus.</h2>
+            <div className='max-w-screen-lg mx-auto max-md:px-4 flex max-md:flex-col items-center  gap-8'>
+                <div className='max-md:order-2'>
+                    <h2 className='text-[7rem] font-semibold leading-none tracking-tighter max-md:text-[6rem]'>ReFokus.</h2>
                 </div>
-                <div className='w-1/2 min-h-[14rem] p-4 flex justify-between items-center'>
+                <div className='w-1/2 min-h-[14rem] max-md:order-1 p-4 max-md:w-full flex justify-between items-center'>
                     <div className='flex flex-col min-h-[10rem] justify-between'>
                         <h3 className='text-zinc-500'>Socials</h3>
                         <div className='flex flex-col'>
