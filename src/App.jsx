b104e4f0ed2a62d971e3d99f01118de350e65ab3
Manish Jha <143255115+maanis@users.noreply.github.com>
2025-02-08 20:36:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
     const scroll = new LocomotiveScroll();
     return (
         <div className="h-full main overflow-x-hidden w-full text-white bg-[#0c0c0c]">
-
             {/* <Cursor /> */}
             <Navbar />
             <Work />
